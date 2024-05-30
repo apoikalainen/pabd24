3806cbb22d60850afe41a915071f6e13f847fa3c
apoikalainen <apoikalainen@gmail.com>
2024-05-30 20:01:51 +0300

    6_tests_for_predict

--- test/test_parallel.py
+++ test/test_parallel.py
@@ -4,7 +4,7 @@ import requests
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
-endpoint = 'http://192.144.14.182:5000/predict'
+endpoint = 'http://176.123.163.78:8000/predict'
 HEADERS = {"Authorization": f"Bearer {config['APP_TOKEN']}"}
 
 
